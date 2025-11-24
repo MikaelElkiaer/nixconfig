6053e27e7dc17650aa98e2a98a23f58b6a052166
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2025-11-24 21:56:36 +0100

    home: Remove upgrademux

--- modules/home/services.nix
+++ modules/home/services.nix
@@ -56,42 +56,6 @@
       };
       enable = true;
     };
-    upgrade-status-hm = {
-      config = {
-        EnvironmentVariables = {
-          PATH = "${
-            lib.makeBinPath [
-              pkgs.bash
-              pkgs.home-manager
-              pkgs.nix
-              pkgs.uutils-coreutils-noprefix
-            ]
-          }:/usr/local/bin:/usr/bin";
-        };
-        Program = "${config.home.homeDirectory}/bin/upgrade-status-hm";
-        RunAtLoad = true;
-        StartInterval = 600;
-      };
-      enable = true;
-    };
-    upgrade-status-nvim = {
-      config = {
-        EnvironmentVariables = {
-          PATH = "${
-            lib.makeBinPath [
-              pkgs.bash
-              pkgs.gnused
-              pkgs.neovim
-              pkgs.uutils-coreutils-noprefix
-            ]
-          }:/usr/local/bin:/usr/bin";
-        };
-        Program = "${config.home.homeDirectory}/bin/upgrade-status-nvim";
-        RunAtLoad = true;
-        StartInterval = 600;
-      };
-      enable = true;
-    };
   };
   systemd.user = {
     services = {
@@ -120,41 +84,6 @@
         Service.ExecStart = "${config.home.homeDirectory}/bin/login-status-gkd";
         Unit.Description = "Set login status for gnome-keyring-daemon";
       };
-      upgrade-status-apt = {
-        Install.WantedBy = [ "default.target" ];
-        Service.ExecStart = "${config.home.homeDirectory}/bin/upgrade-status-apt";
-        Unit.Description = "Set upgrade status for apt";
-      };
-      upgrade-status-hm = {
-        Install.WantedBy = [ "default.target" ];
-        Service = {
-          Environment = [
-            "PATH=${
-              lib.makeBinPath [
-                pkgs.bash
-                pkgs.home-manager
-                pkgs.nix
-              ]
-            }:/usr/local/bin:/usr/bin"
-          ];
-          ExecStart = "${config.home.homeDirectory}/bin/upgrade-status-hm";
-        };
-        Unit.Description = "Set upgrade status for Home Manager";
-      };
-      upgrade-status-nvim = {
-        Install.WantedBy = [ "default.target" ];
-        Service = {
-          Environment = [
-            "PATH=${
-              lib.makeBinPath [
-                pkgs.neovim
-              ]
-            }:/usr/local/bin:/usr/bin"
-          ];
-          ExecStart = "${config.home.homeDirectory}/bin/upgrade-status-nvim";
-        };
-        Unit.Description = "Set upgrade status for Neovim";
-      };
     };
     timers = {
       gh-notifications-counter = {
@@ -180,30 +109,6 @@
           Persistent = true;
         };
         Unit.Description = "Run login status for keyring every minute";
-      };
-      upgrade-status-apt = {
-        Install.WantedBy = [ "timers.target" ];
-        Timer = {
-          OnCalendar = "*-*-* *:0/10:00";
-          Persistent = true;
-        };
-        Unit.Description = "Run upgrade status for apt every 10 minutes";
-      };
-      upgrade-status-hm = {
-        Install.WantedBy = [ "timers.target" ];
-        Timer = {
-          OnCalendar = "*-*-* *:0/10:00";
-          Persistent = true;
-        };
-        Unit.Description = "Run upgrade status for Home Manager every 10 minutes";
-      };
-      upgrade-status-nvim = {
-        Install.WantedBy = [ "timers.target" ];
-        Timer = {
-          OnCalendar = "*-*-* *:0/10:00";
-          Persistent = true;
-        };
-        Unit.Description = "Run upgrade status for Neovim every 10 minutes";
       };
     };
   };
